837cd43b246d9d6b90192dad0001cb00f49b70d3
pd1998 <91264819+pd1998@users.noreply.github.com>
2021-09-23 18:21:44 +0530

    Add files via upload

---
+++ TableScript.sql
@@ -0,0 +1,85 @@
+
+CREATE TABLE [dbo].table_AU(
+	[index] [bigint] NULL,
+	[H] [varchar](max) NULL,
+	[Customer_Name] [varchar](max) NULL,
+	[Customer_Id] [bigint] NULL,
+	[Open_Date] [bigint] NULL,
+	[Last_Consulted_Date] [bigint] NULL,
+	[Vaccination_Id] [varchar](max) NULL,
+	[Dr_Name] [varchar](max) NULL,
+	[State] [varchar](max) NULL,
+	[Country] [varchar](max) NULL,
+	[DOB] [bigint] NULL,
+	[Is_Active] [varchar](max) NULL
+);
+
+
+
+CREATE TABLE [dbo].table_IND(
+	[index] [bigint] NULL,
+	[H] [varchar](max) NULL,
+	[Customer_Name] [varchar](max) NULL,
+	[Customer_Id] [bigint] NULL,
+	[Open_Date] [bigint] NULL,
+	[Last_Consulted_Date] [bigint] NULL,
+	[Vaccination_Id] [varchar](max) NULL,
+	[Dr_Name] [varchar](max) NULL,
+	[State] [varchar](max) NULL,
+	[Country] [varchar](max) NULL,
+	[DOB] [bigint] NULL,
+	[Is_Active] [varchar](max) NULL
+);
+
+
+CREATE TABLE [dbo].table_NYC(
+	[index] [bigint] NULL,
+	[H] [varchar](max) NULL,
+	[Customer_Name] [varchar](max) NULL,
+	[Customer_Id] [bigint] NULL,
+	[Open_Date] [bigint] NULL,
+	[Last_Consulted_Date] [bigint] NULL,
+	[Vaccination_Id] [varchar](max) NULL,
+	[Dr_Name] [varchar](max) NULL,
+	[State] [varchar](max) NULL,
+	[Country] [varchar](max) NULL,
+	[DOB] [bigint] NULL,
+	[Is_Active] [varchar](max) NULL
+);
+
+
+
+CREATE TABLE [dbo].table_PHIL(
+	[index] [bigint] NULL,
+	[H] [varchar](max) NULL,
+	[Customer_Name] [varchar](max) NULL,
+	[Customer_Id] [bigint] NULL,
+	[Open_Date] [bigint] NULL,
+	[Last_Consulted_Date] [bigint] NULL,
+	[Vaccination_Id] [varchar](max) NULL,
+	[Dr_Name] [varchar](max) NULL,
+	[State] [varchar](max) NULL,
+	[Country] [varchar](max) NULL,
+	[DOB] [bigint] NULL,
+	[Is_Active] [varchar](max) NULL
+);
+
+
+CREATE TABLE [dbo].table_USA(
+	[index] [bigint] NULL,
+	[H] [varchar](max) NULL,
+	[Customer_Name] [varchar](max) NULL,
+	[Customer_Id] [bigint] NULL,
+	[Open_Date] [bigint] NULL,
+	[Last_Consulted_Date] [bigint] NULL,
+	[Vaccination_Id] [varchar](max) NULL,
+	[Dr_Name] [varchar](max) NULL,
+	[State] [varchar](max) NULL,
+	[Country] [varchar](max) NULL,
+	[DOB] [bigint] NULL,
+	[Is_Active] [varchar](max) NULL
+);
+
+
+
+Select * from DEPARTMENT1
